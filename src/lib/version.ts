8dd365b5b13b0e2957b8cd5bfe93c0c358d177a1
Robert Rypuła <robert.rypula@gmail.com>
2018-08-15 13:06:07 +0200

    2.1.1

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,5 +1,5 @@
 // Copyright (c) 2018 Robert Rypuła - https://github.com/robertrypula/terminal-game-io
 
-export const version = '2.1.0';
+export const version = '2.1.1';
 export const author = 'Robert Rypuła';
 export const githubUrl = 'https://github.com/robertrypula';
